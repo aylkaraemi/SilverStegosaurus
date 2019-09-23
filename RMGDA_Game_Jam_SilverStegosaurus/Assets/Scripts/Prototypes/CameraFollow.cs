a126f91b9e8afb5b94163ab4d8026ee99782e9ba
Ian <russmoonbase@gmail.com>
2019-09-22 22:17:22 -0600

    Added Isometric prototype scene with point and click movement (NavmeshAgent)

---
+++ RMGDA_Game_Jam_SilverStegosaurus/Assets/Scripts/Prototypes/CameraFollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+   GameObject thePlayer;
+
+   private void Awake()
+   {
+      thePlayer = GameObject.FindGameObjectWithTag("Player");
+   }
+
+   // Start is called before the first frame update
+   void Start()
+   {
+
+   }
+
+   // Update is called once per frame
+   void Update()
+   {
+      this.transform.position += (thePlayer.transform.position - this.transform.position) * 0.05f;
+   }
+}
---
+++ RMGDA_Game_Jam_SilverStegosaurus/Assets/Scripts/Prototypes/PointClickMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PointClickMovement : MonoBehaviour
+{
+   public int moveMask;
+   public NavMeshAgent agent;
+
+   private void Awake()
+   {
+      moveMask = LayerMask.GetMask("MoveArea");
+      agent = GetComponent<NavMeshAgent>();
+   }
+
+
+   // Start is called before the first frame update
+   void Start()
+   {
+
+   }
+
+   // Update is called once per frame
+   void Update()
+   {
+      if (Input.GetMouseButton(0))
+      {
+         MoveToClickLocation();
+      }
+   }
+
+   void MoveToClickLocation()
+   {
+      Ray cameraRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+      RaycastHit hit;
+
+      if (Physics.Raycast(cameraRay, out hit, 500f, moveMask))
+      {
+         agent.SetDestination(hit.point);
+      }
+   }
+}
