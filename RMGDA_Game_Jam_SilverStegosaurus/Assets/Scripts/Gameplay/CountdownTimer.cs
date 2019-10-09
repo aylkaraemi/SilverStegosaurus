062305d333abbdd8702420f41ef201fd26d8bc6d
Kayla Carpenter <aylkaraemi@gmail.com>
2019-10-08 21:49:38 -0600

    Add countdown timer functionality

--- RMGDA_Game_Jam_SilverStegosaurus/Assets/Scripts/Gameplay/CountdownTimer.cs
+++ RMGDA_Game_Jam_SilverStegosaurus/Assets/Scripts/Gameplay/CountdownTimer.cs
@@ -1,18 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CountdownTimer : MonoBehaviour
 {
+    public static float timeLeft = 300.0f; // time left in seconds
+    public Text countdown;
+    private static float minLeft = Mathf.Floor(timeLeft / 60); // minutes left
+    private static float secLeft = Mathf.Floor(timeLeft%60); // seconds left
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        countdown.text = string.Format("{0}:{1:00}", minLeft, secLeft);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timeLeft -= Time.deltaTime;
+        minLeft = Mathf.Floor(timeLeft / 60);
+        secLeft = Mathf.Floor(timeLeft % 60);
+        countdown.text = string.Format("{0}:{1:00}", minLeft, secLeft);
     }
 }
