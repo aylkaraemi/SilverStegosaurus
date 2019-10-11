7cb62d2f1e835811a14e705d402e6bf423a58590
Ian <russmoonbase@gmail.com>
2019-10-11 00:35:58 -0600

    Updated Player model

---
+++ RMGDA_Game_Jam_SilverStegosaurus/Assets/Scripts/Managers/SceneLoader.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+   private static SceneLoader _instance;
+
+   public static SceneLoader Instance
+   {
+      get
+      {
+         if (_instance == null)
+         {
+            Debug.Log("SceneLoader did not load");
+         }
+
+         return _instance;
+      }
+   }
+
+   private void Awake()
+   {
+      _instance = this;
+   }
+
+   public void LoadNextScene()
+   {
+      int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+      SceneManager.LoadScene(currentSceneIndex + 1);
+   }
+
+}
