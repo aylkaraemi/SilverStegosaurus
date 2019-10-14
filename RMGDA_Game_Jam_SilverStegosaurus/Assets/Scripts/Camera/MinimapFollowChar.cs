c5aba71034fb141e1191ef85a25f599e3f1313ad
Kayla Carpenter <aylkaraemi@gmail.com>
2019-10-14 16:41:26 -0600

    Add minimap to main scene

---
+++ RMGDA_Game_Jam_SilverStegosaurus/Assets/Scripts/Camera/MinimapFollowChar.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapFollowChar : MonoBehaviour
+{
+    public Transform playerTransform;
+    public float offset_y = 10f;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = playerTransform.position + Vector3.up * offset_y;
+    }
+}
