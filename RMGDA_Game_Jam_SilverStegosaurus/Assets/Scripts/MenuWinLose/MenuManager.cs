668a726a17da24d3c9f2e1f29277192dd489f36c
Ian <russmoonbase@gmail.com>
2019-10-15 02:39:29 -0600

    Added Main Menu and updated Hope FSM

---
+++ RMGDA_Game_Jam_SilverStegosaurus/Assets/Scripts/MenuWinLose/MenuManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+   public SceneLoader sceneLoader; // loaded in Inspector
+
+   // Start is called before the first frame update
+   void Start()
+   {
+
+   }
+
+   // Update is called once per frame
+   void Update()
+   {
+      if (Input.GetButtonDown("Fire1"))
+      {
+         if (sceneLoader != null)
+         {
+            sceneLoader.LoadNextScene();
+         }
+         
+      }
+   }
+}
