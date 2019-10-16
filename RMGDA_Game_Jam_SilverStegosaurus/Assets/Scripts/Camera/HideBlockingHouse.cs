8fce7c3ae66c9137a0f519d3cd7b5419e8b9afdd
Ian <russmoonbase@gmail.com>
2019-10-16 00:28:40 -0600

    Added new Hope with Skeleton texture to Main Level

---
+++ RMGDA_Game_Jam_SilverStegosaurus/Assets/Scripts/Camera/HideBlockingHouse.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideBlockingHouse : MonoBehaviour
+{
+   private MeshRenderer _meshRenderer;
+
+   private void Start()
+   {
+      _meshRenderer = GetComponent<MeshRenderer>();
+   }
+
+   private void OnTriggerEnter(Collider other)
+   {
+      if (other.tag == "Player")
+      {
+         _meshRenderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.ShadowsOnly;
+      }
+   }
+
+   private void OnTriggerExit(Collider other)
+   {
+      if (other.tag == "Player")
+      {
+         _meshRenderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+      }
+   }
+}
---
+++ RMGDA_Game_Jam_SilverStegosaurus/Assets/Scripts/House/FadeInOutHouse.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeInOutHouse : MonoBehaviour
+{
+   public Material[] originalMaterials;
+   public Material[] fadeMaterials;
+   private MeshRenderer _meshRenderer;
+
+   private void Start()
+   {
+      _meshRenderer = GetComponent<MeshRenderer>();
+      originalMaterials = _meshRenderer.materials;
+   }
+
+
+   private void ChangeToFadeMaterial()
+   {
+      _meshRenderer.materials = fadeMaterials;
+   }
+
+   private void FadeOutMaterials()
+   {
+      foreach(Material mat in _meshRenderer.materials)
+      {
+         StartCoroutine(FadeOutCouroutine(mat));
+      }
+   }
+
+   private IEnumerator FadeOutCouroutine(Material mat)
+   {
+      float timer = 0f;
+      Color matColor = mat.color;
+
+      while (timer < 2.0f)
+      {
+         matColor.a = Mathf.Lerp(1f, 0.3f, 1.0f * Time.deltaTime);
+         yield return null;
+      }
+
+   }
+
+   private void OnTriggerEnter(Collider other)
+   {
+      if (other.tag == "Player")
+      {
+         Debug.Log("Player entered");
+         ChangeToFadeMaterial();
+         //FadeOutMaterials();
+      }
+   }
+}
